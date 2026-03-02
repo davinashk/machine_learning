7786190ee67ca86d773fa06be60d20490837dccf
Avinash Dandothkar <dandothkar.avinash@gmail.com>
2026-03-02 19:25:17 +0000

    update main.py: batch traininf of model

--- main.py
+++ main.py
@@ -2,30 +2,24 @@ import torch
 from torch import nn
 
 
-x1=torch.tensor([[10.0]],dtype=torch.float32)
-y1=torch.tensor([[50.0]],dtype=torch.float32)
-x2=torch.tensor([[37.8]],dtype=torch.float32)
-y2=torch.tensor([[100.0]],dtype=torch.float32)
-  # shape (batch_size=1, in_features=1)
+x=torch.tensor([[10.0],
+                [37.8]],dtype=torch.float32)
+y=torch.tensor([[50.0],
+                [100.0]],dtype=torch.float32)
+
 model=nn.Linear(1,1)
 
 loss_func=nn.MSELoss()
 optimizer=torch.optim.SGD(model.parameters(), lr=0.0001)
 
-for epoch in range(100000):
-    y_pred=model(x1)
-    loss=loss_func(y_pred, y1)
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-
-    y_pred=model(x1)
-    loss=loss_func(y_pred, y1)
+for epoch in range(150000):
+    y_pred=model(x)
+    loss=loss_func(y_pred, y)
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
     
-    if epoch % 10000 == 0:
+    if epoch % 1000 == 0:
         print(f"Epoch {epoch}, Loss: {loss.item()}")
         print(model.weight.item(), model.bias.item())
 
